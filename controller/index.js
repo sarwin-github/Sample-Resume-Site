9e63f2ee244c24756293539fc66362beb681fdf6
sarwin-github <dapito.sherwin@yahoo.com>
2017-02-10 18:05:26 +0800

    Adjusted Layout

--- controller/index.js
+++ controller/index.js
@@ -4,17 +4,19 @@ var fs = require('fs');
 
 // Create endpoint /index/ for getting and displaying initial page ---------------------------------------------------------
 exports.getIndex = ((request, response) => {
-  	var bg_image = ('public/images/background.jpg');
-  	var profile = ('public/images/profile.jpg');
-  		var webTechLogo = ('public/images/webtech.png');
-  			var databaseLogo = ('public/images/database.png');
-  				var FrameworkLogo = ('public/images/framework.png');
+	var bg_image = ('public/images/background.jpg');
+	var profile = ('public/images/profile.jpg');
+	var webTechLogo = ('public/images/webtech.png');
+	var databaseLogo = ('public/images/database.png');
+	var FrameworkLogo = ('public/images/framework.png');
+	var resumeLogo = ('public/images/resume.png');
 
-	    response.render('index.ejs',{ 
-	    	bg_image:bg_image, 
-	    	profile:profile, 
-	    	webTechLogo:webTechLogo, 
-	    	databaseLogo:databaseLogo, 
-	    	FrameworkLogo:FrameworkLogo, });    
+	response.render('index.ejs',{ 
+		bg_image:bg_image, 
+		profile:profile, 
+		webTechLogo:webTechLogo, 
+		databaseLogo:databaseLogo, 
+		FrameworkLogo:FrameworkLogo,
+		resumeLogo: resumeLogo });    
 });
 
